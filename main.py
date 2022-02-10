dac11532a2826612091796217eae74adf69e5b25
Ance Spolite <97155723+ancespp@users.noreply.github.com>
2022-02-10 06:31:28 +0000

    Pabeigts kods

--- main.py
+++ main.py
@@ -1,36 +1,76 @@
-n=0
+import os
+
+print("Mēs strādājam no 10:00 līdz 22:00, bet piektdienās līdz 20:00")
+print()
+saz=int(input("Vai jūs vēlaties lai mēs sazināmies ar Jums caur telefonu (ievadiet 1) vai caur e-pastu(Ievadiet 2)?: "))
+if saz==1:
+  tlf=int(input("Ievadi savu telefona numuru: "))
+elif saz==2:
+  epast=input("Ievadi savu e-pastu: ")
+else:
+  print("Tā nav viena no izvēlēm!")
+  exit()
 vards=input("Ievadi savu vārdu:")
 uzvards=input("Ievadi savu uzvārdu: ")
 dzim=input("Ievadi dzimumu: ")
-tlf=int(input("Ievadi savu telefona numuru: "))
-epast=input("Ievadi savu e-pastu: ")
 meist=input("Ir pieejami divi meistari-Juris (nagu meistars) un Laimdota (friziere), kurš būs Jūsu meistars?: ")
 if meist=="Juris" or meist=="Laimdota":
-  print("ok")
-  n+=1
+  print("Meistars izvēlēts!")
 else:
   print("Tada meistara nav!")
   exit()
-diena=float(input("Kurā dienā Jūs vēlētos ierasties?: "))
-laiks=input("cikos Jūs vēlētos ierasties?: ")
-n=open("numurs.txt", "r")
-num=n.read()
-n.close
-n=open("numurs.txt", "w")
-n.write(f"{n+1}")
-n.close()
+
+
+if meist=="Juris":
+  jrs = open("Juris.txt", "r")
+  content = jrs.read()
+  print(f"Šie laiki Jurim nav pieejami: {content}")
+  content_list = content.split(",")
+  jrs.close()
+  diena=float(input("Kurā dienā Jūs vēlētos ierasties? (Piem. 21.09): "))
+  laiks=input("cikos Jūs vēlētos ierasties? Meistara pieejamība ir tikai pa veselai stundai (Piem. 14:00):  ")
+  n=open("Juris.txt", "a")
+  n.write(f"({diena} {laiks}) \n")
+  n.write(" ")
+  n.close()
+
+else:
+  lmd=open("Laimdota.txt", "r")
+  contents=lmd.read()
+  print(f"Šie laiki Laimdotai nav pieejami: {contents}")
+  conteta_lists=contents.split(",")
+  lmd.close()
+  diena=float(input("Kurā dienā Jūs vēlētos ierasties? (Piem. 21.09): "))
+  laiks=input("cikos Jūs vēlētos ierasties? Meistara pieejamība ir tikai pa veselai stundai (Piem. 14:00): ")
+  n=open("Laimdota.txt", "a")
+  n.write(f"({diena} {laiks}) \n")
+  n.write(" ")
+  n.close()
+
 
 f=open("lietotajs.txt", "a")
 f.write(" ")
-f.write(f"{n}. apmeklētājs \n")
 f.write(f" Vārds: {vards} \n")
 f.write(f" Uzvārds: {uzvards} \n")
 f.write(f" Dzimums: {dzim}\n")
-f.write(f" Telefona numurs: {tlf} \n")
-f.write(f" E-pasts: {epast} \n")
+if saz==1:
+  f.write(f" Telefona numurs: {tlf} \n")
+else:
+  f.write(f" E-pasts: {epast} \n")
 f.write(f" Izvēlētais meistars: {meist} \n")
 f.write(f" Datums: {diena} \n")
 f.write(f" Izvēlētais laiks: {laiks} \n")
-f.write("")
+f.write(" ")
 
 f.close()
+print()
+
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  
+        command = 'cls'
+    os.system(command)
+
+clearConsole()
+print("Paldies ka izvēlējāties Jūčūb skaistumkopšanas salonu!")
